c62b9c000da0b1c61384c8a027d5877605a48cce
justinamaze <justinamaze1990@gmail.com>
2021-01-24 20:22:02 -0500

    adding the deliverables

---
+++ Employee_Database_challenge.sql
@@ -0,0 +1,33 @@
+--MODULE 7, DELIVERABLE 1
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+    t.title,
+	t.from_date,
+	t.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as t
+ON e.emp_no = t.emp_no
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY emp_no;
+		
+
+SELECT DISTINCT ON (rt.emp_no)
+		rt.emp_no,
+		rt.first_name,
+		rt.last_name,
+		rt.title
+INTO unique_titles
+FROM retirement_titles AS rt
+ORDER BY emp_no ASC, to_date DESC;
+SELECT * FROM unique_titles;
+
+SELECT COUNT (ut.title), ut.title
+INTO retiring_titles
+FROM unique_titles AS ut
+GROUP BY ut.title
+ORDER BY COUNT DESC; 
+
+
+
